15b2e89feebe2bc8577e40398a6c65b91011a3c3
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2024-06-26 21:10:39 +0200

    Added service worker to allow for offline usage

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 import { AppRoutingModule } from './app/app-routing.module';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(AppRoutingModule)],
+  providers: [
+    importProvidersFrom(AppRoutingModule),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 });
