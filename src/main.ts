3ff9938a814560d06ac67ad2587a122263f929fa
Hoang Duc Nguyen <hoang.d1997@gmail.com>
2025-12-29 00:50:25 +0100

    Updated angular v20 to v21

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { importProvidersFrom, isDevMode } from '@angular/core';
+import { importProvidersFrom, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 import { AppRoutingModule } from './app/app-routing.module';
@@ -6,7 +6,7 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(AppRoutingModule),
+    provideZoneChangeDetection(),importProvidersFrom(AppRoutingModule),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
